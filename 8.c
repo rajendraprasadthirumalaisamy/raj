2aee61fa5e816a75fbebb8ec039dc6afe913ad53
rajendraprasadthirumalaisamy <31513315+rajendraprasadthirumalaisamy@users.noreply.github.com>
2018-03-12 16:21:26 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char name[20];
+	int j;
+	scanf("%[^\n]s",name);
+		printf("%s\n",name);
+	name[0]=name[0]-32;
+	for(j=0;name[j]!='\0';j++)
+	{
+		if(name[j]==' ')
+	{j++;
+	name[j]=name[j]-32;}
+	}
+	printf("%s",name);
+		
+}
